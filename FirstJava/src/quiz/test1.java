d1aa28e8d550b6fd46c9a0f9febcfd7922a7104a
jo sungwoo <jo sungwoo@host.docker.internal>
2024-03-07 17:41:19 +0900

    240307 수업끝!

--- FirstJava/src/quiz/test1.java
+++ FirstJava/src/quiz/test1.java
@@ -3,24 +3,24 @@ package quiz;
 public class test1 {
 
 	public static void main(String[] args) {
-		System.out.println("1번  ===================");
-		System.out.println("           /)/)        ");
-		System.out.println("          (  ..)       ");
-		System.out.println("          (  >♡        ");
-		System.out.println("     Have a Good Time. ");
-		System.out.println("    ===================");
+		System.out.println("===================");
+		System.out.println("       /)/)");
+		System.out.println("      (  ..)");
+		System.out.println("      (   >♡");
+		System.out.println(" Have a Good Time.");
+		System.out.println("===================");
 		
 
-		System.out.println("2번                     ####회비 정보####                        ");
-		System.out.println("      ========================================================");
-		System.out.println("      이름		나이	    전화번호	    회비        ");
-		System.out.println("      ========================================================");
-		System.out.println("      홍길동	        \"15\"        010-123-1234    ￦20,000    ");
-		System.out.println("      임꺽정	        \"20\"        010-234-2345    ￦30,000    ");
-		System.out.println("      김말이	        \"28\"        010-345-3456    ￦30,000    ");
-		System.out.println("      --------------------------------------------------------");
-		System.out.println("      총합계                                          ￦100,000 ");
-		System.out.println("      ========================================================");
+		System.out.println("           #### 회비 정보 ####");
+		System.out.println("==========================================");
+		System.out.println("이름\t나이\t전화번호\t\t회비");
+		System.out.println("==========================================");
+		System.out.println("홍길동\t\"15\"\t\"010-123-1234\"\t₩20,000");
+		System.out.println("임꺽정\t\"20\"\t\"010-234-2345\"\t₩30,000");
+		System.out.println("김말이\t\"28\"\t\"010-345-3456\"\t₩50,000");
+		System.out.println("------------------------------------------");
+		System.out.println("총합계\t\"15\"\t\t\t₩100,000");
+		System.out.println("==========================================");
 		
 		System.out.println("");
 		
