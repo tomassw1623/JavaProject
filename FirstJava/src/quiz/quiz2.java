1d21b8a6a5f658ce9c8b0338156df5e520794f56
jo sungwoo <jo sungwoo@172.171.201.99>
2024-03-11 21:31:13 +0900

    복습

--- FirstJava/src/quiz/quiz2.java
+++ FirstJava/src/quiz/quiz2.java
@@ -4,23 +4,21 @@ public class quiz2 {
 
 	public static void main(String[] args) {
 		
-		String banner = "*****출력 결과 *****";
+		String banner = "* * * * * 출력 결과 * * * * *";
 		String name = "홍길동";
 		int age = 20;
 		String tel = "010-1234-1234";
-		double height = 175.5;
+		double height = 178.5;
 		double weight = 75;
 		String bloodType = "O";
 		
-		System.out.printf(banner);
-		System.out.printf("이름\t: %s\n",name);
-		System.out.printf("나이\t: %ㅇ\n",age);
-		System.out.printf("Tel\t: %s\n",tel);
-		System.out.printf("키\t: %f\n",height);
-		System.out.printf("몸무게\t: %f\n",weight);
-		System.out.printf("이름\t: %s\n",bloodType);
-		
-		
+	    System.out.println(banner);
+	    System.out.printf("이름\t: %s\n",name);
+	    System.out.printf("나이\t: %d\n",age);
+	    System.out.printf("Tel\t: %s\n",tel);
+	    System.out.printf("키\t: %f\n",height);
+	    System.out.printf("몸무게\t: %f\n",weight);
+	    System.out.printf("혈액형\t: %s\n",bloodType);
 		
 
 	}
